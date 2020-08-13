d1aab6b76b9514ab94f12fcfe9aa6acb21abd025
Leonard Ehrenfried <mail@leonard.io>
2020-08-13 15:13:56 +0200

    Display result of onLocationSelected

--- src/widget.js
+++ src/widget.js
@@ -3,19 +3,23 @@ class StadtnaviWidget {
   defaults = {
     tileUrl: "https://tiles.stadtnavi.eu/streets/{z}/{x}/{y}{r}.png",
     center: [48.7840, 9.1829],
+    onLocationSelected: () => {}
   };
 
 
-  constructor(divId) {
+  constructor(divId, options) {
+
+    const mergedOptions = this.computeOptions(options || {});
 
     const map = L.map(divId, {
-      center: this.defaults.center,
+      center: mergedOptions.center,
       zoom: 12
     });
 
+
     var markers = [];
 
-    L.tileLayer(this.defaults.tileUrl, {
+    L.tileLayer(mergedOptions.tileUrl, {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       maxZoom: 18,
       tileSize: 256
@@ -23,15 +27,23 @@ class StadtnaviWidget {
 
     map.on("click", (e) => {
 
-
       markers.forEach(m => map.removeLayer(m));
       const marker = L.marker(e.latlng).addTo(map);
       markers = [marker];
 
       const { lat, lng } = e.latlng;
 
-      this.reverseGeocode(lat, lng).then(address => marker.bindPopup(address).openPopup());
+      this.reverseGeocode(lat, lng)
+        .then(address => {
+          marker.bindPopup(address).openPopup();
 
+          mergedOptions.onLocationSelected({
+            address,
+            coordinates: {
+              lat, lng
+            }
+          });
+        });
     });
   }
 
@@ -53,5 +65,18 @@ class StadtnaviWidget {
 
     const address = `${firstPart}, ${postcode} ${city}`;
     return address;
+  }
+
+  computeOptions(options) {
+    const ret = {};
+    Object.keys(this.defaults).forEach(key => {
+      const userValue = options[key]
+      if(userValue) {
+        ret[key] = userValue;
+      } else {
+        ret[key] = this.defaults[key];
+      }
+    });
+    return ret;
   }
 }
