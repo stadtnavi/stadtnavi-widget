68a1c6d72115bccfc28fbefa97793641e03173c5
leonardehrenfried <mail@leonard.io>
2022-06-15 10:59:49 +0000

    Deploying to gh-pages from  @ 9a5a54b73200ceeaef905eca6fa71cfca6f932ce 🚀

--- latest/tiny-widget.js
+++ latest/tiny-widget.js
@@ -637,42 +637,79 @@
 
 class TinyAddressBox {
 
-  makeAutoComplete() {
+  makeAutoComplete(parent, placeholder) {
+    const div = document.createElement('div');
+    parent.appendChild(div);
     const input = document.createElement('input');
-    new autoComplete({
-      placeHolder: "Search for Food...",
+    div.appendChild(input);
+
+    const autoCompleteObj = new autoComplete({
+      placeHolder: placeholder,
       selector: () => {
         return input;
       },
-      data: {
-        src: ["Sauce - Thousand Island", "Wild Boar - Tenderloin", "Goat - Whole Cut"],
-        cache: true,
-      }
-    })
-    return input;
+      data:{
+        src: (query) => {
+          const peliasUrl = `https://photon.stadtnavi.eu/pelias/v1/search?text=${query}&focus.point.lat=53.015895&focus.point.lon=14.000255&layers=station,venue,address,street&lang=de`;
+          return fetch(`${peliasUrl}`)
+            .then(r => r.json())
+            .then(r => r.features.map(this.toDisplayValues));
+        }
+      },
+      searchEngine: (query, record) => {
+        return record.name;
+      },
+      submit: true,
+      threshold: 3,
+      debounce: 150,
+
+      events: {
+        input: {
+          selection(event) {
+            const feedback = event.detail;
+            autoCompleteObj.input.blur();
+            // Replace Input value with the selected value
+            autoCompleteObj.input.value = feedback.selection.match;
+            autoCompleteObj.selection = feedback.selection;
+          },
+        },
+      },
+    });
+
+    return autoCompleteObj;
+  }
+
+  toDisplayValues(f) {
+    return {
+      name: f.properties.label,
+      coordinates: f.geometry.coordinates
+    };
+  }
+
+  toUrlPart(selection) {
+    if(!selection) {
+      return "-";
+    }
+    const [lng, lat] = selection.value.coordinates;
+    return `${selection.match}::${lat},${lng}`
   }
 
   constructor(divId, options) {
 
-    const peliasUrl = "https://photon.stadtnavi.eu/pelias/v1/search?text=herz&boundary.rect.min_lat=50.015895&boundary.rect.max_lat=54.015895&boundary.rect.min_lon=15.000255&boundary.rect.max_lon=11.000255&focus.point.lat=53.015895&focus.point.lon=14.000255&lang=de&sources=oa%2Cosm%2Cgtfsbbnavi&layers=station%2Cvenue%2Caddress%2Cstreet";
     const container = document.getElementById(divId);
 
-    container.appendChild(this.makeAutoComplete());
+    var start = this.makeAutoComplete(container, "Startort eingeben");
+    var end = this.makeAutoComplete(container, "Zielort eingeben");
 
-    this.defaults = {
-      ... {
-        center: { lat: 48.7840, lng: 9.1829 },
-      }
+    const button = document.createElement('button');
+    button.textContent = "Route suchen"
+
+    container.appendChild(button);
+
+    button.onclick = () => {
+      const first = this.toUrlPart(start.selection);
+      const second = this.toUrlPart(end.selection);
+      window.open(`https://angermuende.bbnavi.de/${first}/${second}`)
     };
-
-
-    fetch(`${peliasUrl}`)
-      .then(r => r.json())
-      .then(json => {
-
-        const latLng = json.features[0].geometry.coordinates;
-        latLng.reverse();
-      });
   }
-
 }
